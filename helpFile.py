6edb9932bb03c861fdbe1487aa386ecd8cf16032
Sergio Yahni <sergioyahni@gmail.com>
2024-02-06 15:39:47 +0200

    Create helpFile.py

---
+++ helpFile.py
@@ -0,0 +1,16 @@
+helpText = """
+1. To show all passwords: 
+   $ psw show
+
+2. To search for credentials of a specific website: 
+   $ psw search website.com
+
+3. To save credentials for a website:
+   - If you have a password you want to use: 
+     $ psw save website.com user@email.com your_password
+   - If you want to generate a strong password: 
+     $ psw save website.com user@email.com
+
+4. To delete credentials for a website:
+   $ psw delete website.com
+"""
